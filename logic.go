bfe3e7c3ed4b6180ceca9c4020fdfcc4ab298c4c
dylan-arts <86665189+dylan-arts@users.noreply.github.com>
2022-01-11 09:45:28 +0100

    Fixed most code duplication

--- logic.go
+++ logic.go
@@ -5,6 +5,7 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"reflect"
@@ -21,36 +22,8 @@ func NewEncryptionService(key []byte) EncryptionService {
 	}
 }
 
-//Get encrypted []byte by inputting string
-func (e *encryptionService) EncryptStr(str string) ([]byte, error) {
-	//Create new cipher using given key
-	block, err := aes.NewCipher(e.key)
-	if err != nil {
-		return nil, err
-	}
-
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-
-	//Create random nonce so that the same input value changes when it is encrypted
-	nonce := make([]byte, aesGCM.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-
-	val := aesGCM.Seal(nonce, nonce, []byte(str), nil) //Encrypt using all values
-
-	return val, nil
-}
-
-//Encrypt any 1-dimensional struct and get an interface ready for MongoDB storage
-func (e *encryptionService) EncryptToInterface(eData interface{}) (map[string]interface{}, error) {
-	//Get underlying object of interface
-	object := reflect.ValueOf(eData)
-	returnObj := map[string]interface{}{}
-
+//Helper functions to remove code duplication
+func (e *encryptionService) initGCM() (cipher.AEAD, error) {
 	//Create cipher with given key
 	block, err := aes.NewCipher(e.key)
 	if err != nil {
@@ -62,35 +35,77 @@ func (e *encryptionService) EncryptToInterface(eData interface{}) (map[string]in
 		return nil, fmt.Errorf("failed to create GCM, external cipher package returned the following error: %s", err)
 	}
 
+	return aesGCM, nil
+}
+
+func (e *encryptionService) sealWithNonce(aesGCM cipher.AEAD, str string) ([]byte, error) {
+	//Create random nonce so that the same input value changes when it is encrypted
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	val := aesGCM.Seal(nonce, nonce, []byte(str), nil) //Encrypt using all values
+
+	return val, nil
+}
+
+func (e *encryptionService) findFieldTag(object reflect.StructTag, fieldTagNames []string) (string, error) {
+	for i := 0; i < len(fieldTagNames); i++ {
+		field := object.Get(fieldTagNames[i])
+
+		if field != "" && len(field) != 0 {
+			return field, nil
+		}
+	}
+
+	return "", errors.New("none of the necessary field tags were found in the structure could not map to interface")
+}
+
+//END
+
+//Get encrypted []byte by inputting string
+func (e *encryptionService) EncryptStr(str string) ([]byte, error) {
+	aesGCM, err := e.initGCM()
+	if err != nil {
+		return nil, err
+	}
+
+	val, err := e.sealWithNonce(aesGCM, str)
+	if err != nil {
+		return nil, err
+	}
+
+	return val, nil
+}
+
+//Encrypt any 1-dimensional struct and get an interface ready for MongoDB storage
+func (e *encryptionService) EncryptToInterface(eData interface{}) (map[string]interface{}, error) {
+	//Get underlying object of interface
+	object := reflect.ValueOf(eData)
+	returnObj := map[string]interface{}{}
+
+	aesGCM, err := e.initGCM()
+	if err != nil {
+		return nil, err
+	}
+
 	//For each field in object
 	for i := 0; i < object.NumField(); i++ {
-
-		//Create random nonce so that the same input value changes when it is encrypted
-		nonce := make([]byte, aesGCM.NonceSize())
-		if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-			panic(err.Error())
-		}
-
-		var fieldName string
-
-		//Get field name by reading bson tag
-		fieldName = object.Type().Field(i).Tag.Get("bson")
-		//Check if bson tag was empty
-		if fieldName == "" || len(fieldName) == 0 {
-			//Check if ename is present if bson wasn't
-			fieldName = object.Type().Field(i).Tag.Get("ename")
-		}
-
-		//If none of the tags were present error
-		if len(fieldName) == 0 {
-			return nil, fmt.Errorf("the provided struct does not have either the bson or ename tag, this is necessary for creating return map")
+		fieldName, err := e.findFieldTag(object.Type().Field(i).Tag, []string{"bson", "ename"})
+		if err != nil {
+			return nil, err
 		}
 
 		//Get encrypted tag
 		encrypt := object.Type().Field(i).Tag.Get("encrypted")
 		//If encrypted == false don't encrypt otherwise encrypt
 		if encrypt != "false" {
-			val := aesGCM.Seal(nonce, nonce, []byte(fmt.Sprint(object.Field(i))), nil)
+			val, err := e.sealWithNonce(aesGCM, fmt.Sprint(object.Field(i)))
+			if err != nil {
+				return nil, err
+			}
+
 			returnObj[fieldName] = val
 		} else {
 			val := fmt.Sprint(object.Field(i))
@@ -108,44 +123,25 @@ func (e *encryptionService) EncryptToJSON(eData interface{}) ([]byte, error) {
 	object := reflect.ValueOf(eData)
 	returnObj := map[string]interface{}{}
 
-	//Create cipher with given key
-	block, err := aes.NewCipher(e.key)
-	if err != nil {
-		return nil, err
-	}
-
-	aesGCM, err := cipher.NewGCM(block)
+	aesGCM, err := e.initGCM()
 	if err != nil {
 		return nil, err
 	}
 
 	//For each field in object
 	for i := 0; i < object.NumField(); i++ {
-
-		//Create random nonce so that the same input value changes when it is encrypted
-		nonce := make([]byte, aesGCM.NonceSize())
-		if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-			panic(err.Error())
+		fieldName, err := e.findFieldTag(object.Type().Field(i).Tag, []string{"bson", "json"})
+		if err != nil {
+			return nil, err
 		}
 
-		var fieldName string
-
-		//Get bson tag
-		fieldName = object.Type().Field(i).Tag.Get("bson")
-		//Check if bson tag was empty
-		if fieldName == "" || len(fieldName) == 0 {
-			//If empty get json tag
-			fieldName = object.Type().Field(i).Tag.Get("json")
-		}
-
-		//If no tags were present error
-		if len(fieldName) == 0 {
-			return nil, fmt.Errorf("the provided struct does not have either the json or bson tag, this is necessary for creating return map")
-		}
 		encrypt := object.Type().Field(i).Tag.Get("encrypted")
 
 		if encrypt != "false" {
-			val := aesGCM.Seal(nonce, nonce, []byte(fmt.Sprint(object.Field(i))), nil)
+			val, err := e.sealWithNonce(aesGCM, fmt.Sprint(object.Field(i)))
+			if err != nil {
+				return nil, err
+			}
 			returnObj[fieldName] = val
 		} else {
 			val := fmt.Sprint(object.Field(i))
@@ -168,15 +164,9 @@ func (e *encryptionService) Decrypt(encryptedData interface{}, desiredOutput int
 	object := reflect.ValueOf(encryptedData)
 	returnObj := reflect.New(reflect.ValueOf(desiredOutput).Type())
 
-	//Create new cipher using given key
-	block, err := aes.NewCipher(e.key)
+	aesGCM, err := e.initGCM()
 	if err != nil {
-		return nil, fmt.Errorf("failed to create cipher, external aes package returned the following error: %s", err)
-	}
-
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create GCM, external cipher package returned the following error: %s", err)
+		return nil, err
 	}
 
 	//For each field in object
@@ -213,14 +203,9 @@ func (e *encryptionService) Decrypt(encryptedData interface{}, desiredOutput int
 //Decrypt encrypted []byte of a string
 func (e *encryptionService) DecryptStr(b []byte) (string, error) {
 	//Create cipher using given key
-	block, err := aes.NewCipher(e.key)
+	aesGCM, err := e.initGCM()
 	if err != nil {
-		return "", fmt.Errorf("failed to create cipher, external aes package returned the following error: %s", err)
-	}
-
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		return "", fmt.Errorf("failed to create GCM, external cipher package returned the following error: %s", err)
+		return "", err
 	}
 
 	//Decrypt string
@@ -245,30 +230,21 @@ func (e encryptionService) getPlainText(val []byte, aesGCM cipher.AEAD) (string,
 		return "", err
 	}
 
-	return fmt.Sprintf("%s", plaintext), nil
+	return string(plaintext), nil
 }
 
 //Encrypt []byte
 func (e *encryptionService) EncryptByt(b []byte) ([]byte, error) {
 	//Create new cipher using given key
-	block, err := aes.NewCipher(e.key)
+	aesGCM, err := e.initGCM()
 	if err != nil {
 		return nil, err
 	}
 
-	aesGCM, err := cipher.NewGCM(block)
+	val, err := e.sealWithNonce(aesGCM, string(b))
 	if err != nil {
 		return nil, err
 	}
-
-	//Create nonce
-	nonce := make([]byte, aesGCM.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-
-	//Encrypt using all values
-	val := aesGCM.Seal(nonce, nonce, b, nil)
 
 	return val, nil
 }
@@ -276,14 +252,9 @@ func (e *encryptionService) EncryptByt(b []byte) ([]byte, error) {
 //Decrypt byte
 func (e *encryptionService) DecryptByt(b []byte) ([]byte, error) {
 	//Create new cipher using given key
-	block, err := aes.NewCipher(e.key)
+	aesGCM, err := e.initGCM()
 	if err != nil {
-		return nil, fmt.Errorf("failed to create cipher, external aes package returned the following error: %s", err)
-	}
-
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create GCM, external cipher package returned the following error: %s", err)
+		return nil, err
 	}
 
 	//Decrypt bytes
